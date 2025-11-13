411f4a42a1bc370147d08655c2405d0bf6dab5b4
Cookie  <cookiexlks@gmail.com>
2025-11-13 17:52:13 -0500

    Create Cursor changer.js

---
+++ backgrounds/Cursor changer.js
@@ -0,0 +1,1 @@
+javascript:var c=prompt("What Cursor Do YOu want the choices are monkey, black, rainbow.");(function(){var x = document.getElementsByTagName("*");for (i=0;i<x.length;i++){x[i].style.cursor=c};}());
